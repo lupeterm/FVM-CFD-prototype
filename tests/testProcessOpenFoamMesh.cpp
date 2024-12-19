1ba99dbd9263e4fad2ae4dc48b1b35dbcc66eea2
chihta-wang <wangchihta@gmail.com>
2024-12-19 18:10:54 +0100

    Add the draft implementation of comparison of two floating-point numbers

--- tests/testProcessOpenFoamMesh.cpp
+++ tests/testProcessOpenFoamMesh.cpp
@@ -2,8 +2,37 @@
 
 #include "Mesh.h"
 #include "readMesh.h"
+#include <cmath>
 #include <cstddef>
 #include <string>
+
+// template <typename T1, typename T2>
+// ::testing::AssertionResult
+// AlmostEqualRelativeAndAbs(const T1 &actualValue, const T2 &expectedValue,
+//                           const double maxDiff, const double maxRelativeDiff)
+//                           {
+
+//   bool almostEqual = false;
+
+//   // Check if the numbers are really close when comparing numbers near zero
+//   double absDiff = std::fabs(expectedValue - actualValue);
+//   if (absDiff <= maxDiff) {
+//     almostEqual = true;
+//   }
+
+//   // Check if the numbers are really close when comparing numbers away from
+//   // zero
+//   auto expectedAbsValue = std::fabs(expectedValue);
+//   auto actualAbsValue = std::fabs(actualValue);
+//   auto largerAbsValue =
+//       (expectedAbsValue > actualAbsValue) ? expectedAbsValue :
+//       actualAbsValue;
+
+//   if (absDiff <= largerAbsValue * maxRelativeDiff) {
+//     almostEqual = true;
+//   }
+//   // almostEqual = false;
+// }
 
 TEST(ProcessingOpenFoamMeshTest, ReadingPointsWorks) {
   // --- Arrange ---
