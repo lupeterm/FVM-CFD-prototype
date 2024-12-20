6ad5893810cd64e29b389ec64051322197c91943
chihta-wang <wangchihta@gmail.com>
2024-12-20 16:25:45 +0100

    Fix the bug in the function of processing basic face geometry Add two unit tests for the function

--- src/processMesh.cpp
+++ src/processMesh.cpp
@@ -3,65 +3,119 @@
 #include <cstddef>
 #include <vector>
 
+#include <iostream>
+
+void printOutVector(std::vector<double> &customVector) {
+  for (std::size_t iCoordinate = 0; iCoordinate < customVector.size();
+       ++iCoordinate) {
+    std::cout << customVector[iCoordinate] << "\n";
+  }
+  std::cout << std::endl;
+}
+
 void processMesh::processBasicFaceGeometry(Mesh &fvMesh) {
   std::size_t nFaces = fvMesh.nFaces();
 
   for (std::size_t iFace = 0; iFace < nFaces; ++iFace) {
-    std::size_t *iNodes = fvMesh.faces()[iFace].iNodes();
-    const std::size_t nNodes = fvMesh.faces()[iFace].nNodes();
-
     /*
     // Compute the geometric center of a face
     */
     std::vector<double> center = {0.0, 0.0, 0.0};
-    for (std::size_t iNode = 0; iNode < nNodes; ++iNode) {
+    for (std::size_t iNode = 0; iNode < fvMesh.faces()[iFace].nNodes();
+         ++iNode) {
       for (std::size_t iCoordinate = 0; iCoordinate < center.size();
            ++iCoordinate)
-        center[iCoordinate] += fvMesh.nodes()[iNode].centroid()[iCoordinate];
+        center[iCoordinate] +=
+            fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[iNode]]
+                .centroid()[iCoordinate];
     }
 
     for (std::size_t iCoordinate = 0; iCoordinate < center.size();
          ++iCoordinate) {
-      center[iCoordinate] /= static_cast<double>(nNodes);
+      center[iCoordinate] /=
+          static_cast<double>(fvMesh.faces()[iFace].nNodes());
     }
+
+    //***************** Debug code ******************
+
+    if (iFace == 0) {
+      std::cout << "face 0 static cast<double>(nNode):" << "\n";
+      std::cout << static_cast<double>(fvMesh.faces()[iFace].nNodes())
+                << std::endl;
+
+      std::cout << "face 0 center = " << "\n";
+      printOutVector(center);
+    }
+    //***********************************************
 
     /*
     // Using the center to compute the area and centroid of virtual
     // triangles based on the center and the face nodes
     */
-    std::vector<double> point1 = center;
-    std::vector<double> point2(3, 0.0);
-    std::vector<double> point3(3, 0.0);
+    std::vector<double> triangleNode1 = center;
+    std::vector<double> triangleNode2(3, 0.0);
+    std::vector<double> triangleNode3(3, 0.0);
     std::vector<double> centroid(3, 0.0);
     std::vector<double> Sf(3, 0.0);
     double area = 0.0;
 
     // TO DO: Should consider a special case when the polygon is a triangle
-    for (std::size_t iTriangle = 0; iTriangle < nNodes; ++iTriangle) {
-      point2 = fvMesh.nodes()[iNodes[iTriangle]].centroid();
-      if (iTriangle < nNodes - 1) {
-        point3 = fvMesh.nodes()[iNodes[iTriangle + 1]].centroid();
+    for (std::size_t iNode = 0; iNode < fvMesh.faces()[iFace].nNodes();
+         ++iNode) {
+      triangleNode2 =
+          fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[iNode]].centroid();
+      if (iNode < fvMesh.faces()[iFace].nNodes() - 1) {
+        triangleNode3 =
+            fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[iNode + 1]]
+                .centroid();
       } else {
-        point3 = fvMesh.nodes()[iNodes[0]].centroid();
+        triangleNode3 =
+            fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[0]].centroid();
       }
 
       // Calculate the centroid of a given subtriangle
       std::vector<double> local_centroid(3, 0.0);
-      for (std::size_t iCoordinate = 0; iCoordinate < 3; ++iCoordinate) {
+      for (std::size_t iCoordinate = 0; iCoordinate < local_centroid.size();
+           ++iCoordinate) {
         local_centroid[iCoordinate] =
-            (point1[iCoordinate] + point2[iCoordinate] + point3[iCoordinate]) /
+            (triangleNode1[iCoordinate] + triangleNode2[iCoordinate] +
+             triangleNode3[iCoordinate]) /
             3.0;
       }
+
+      //***************** Debug code ******************
+      if (iFace == 0) {
+        if (iNode == 0) {
+          std::cout << "face 0 triangle 0 centroid:" << std::endl;
+
+        } else if (iNode == 1) {
+          std::cout << "face 0 triangle 1 centroid:" << std::endl;
+
+        } else if (iNode == 2) {
+          std::cout << "face 0 triangle 2 centroid:" << std::endl;
+
+        } else if (iNode == 3) {
+          std::cout << "face 0 triangle 3 centroid:" << std::endl;
+        }
+        printOutVector(local_centroid);
+      }
+      //***********************************************
 
       // Calculate the surface area vector of a given subtriangle by cross
       // product
       std::vector<double> local_Sf(3, 0.0);
-      local_Sf[0] = 0.5 * ((point2[1] - point1[1]) * (point3[2] - point1[2]) -
-                           (point2[2] - point1[2]) * (point3[1] - point1[1]));
-      local_Sf[1] = 0.5 * ((point2[2] - point1[2]) * (point3[0] - point1[0]) -
-                           (point2[0] - point1[0]) * (point3[2] - point1[2]));
-      local_Sf[2] = 0.5 * ((point2[0] - point1[0]) * (point3[1] - point1[1]) -
-                           (point2[1] - point1[1]) * (point3[0] - point1[0]));
+      local_Sf[0] = 0.5 * ((triangleNode2[1] - triangleNode1[1]) *
+                               (triangleNode3[2] - triangleNode1[2]) -
+                           (triangleNode2[2] - triangleNode1[2]) *
+                               (triangleNode3[1] - triangleNode1[1]));
+      local_Sf[1] = 0.5 * ((triangleNode2[2] - triangleNode1[2]) *
+                               (triangleNode3[0] - triangleNode1[0]) -
+                           (triangleNode2[0] - triangleNode1[0]) *
+                               (triangleNode3[2] - triangleNode1[2]));
+      local_Sf[2] = 0.5 * ((triangleNode2[0] - triangleNode1[0]) *
+                               (triangleNode3[1] - triangleNode1[1]) -
+                           (triangleNode2[1] - triangleNode1[1]) *
+                               (triangleNode3[0] - triangleNode1[0]));
 
       // Calculate the surface area of a given subtriangle
       double local_area =
@@ -73,9 +127,8 @@ void processMesh::processBasicFaceGeometry(Mesh &fvMesh) {
         centroid[iCoordinate] += local_area * local_centroid[iCoordinate];
         Sf[iCoordinate] += local_Sf[iCoordinate];
       }
-
-      area += local_area;
     }
+    area = std::sqrt(Sf[0] * Sf[0] + Sf[1] * Sf[1] + Sf[2] * Sf[2]);
 
     // Compute centroid of the polygon
     for (std::size_t iCoordinate = 0; iCoordinate < centroid.size();
--- tests/testProcessOpenFoamMesh.cpp
+++ tests/testProcessOpenFoamMesh.cpp
@@ -4,46 +4,74 @@
 #include "readMesh.h"
 #include <cmath>
 #include <cstddef>
+#include <iostream>
 #include <string>
 
-// template <typename T1, typename T2>
-// ::testing::AssertionResult
-// AlmostEqualRelativeAndAbs(const T1 &actualValue, const T2 &expectedValue,
-//                           const double maxDiff, const double maxRelativeDiff)
-//                           {
-
-//   bool almostEqual = false;
-
-//   // Check if the numbers are really close when comparing numbers near zero
-//   double absDiff = std::fabs(expectedValue - actualValue);
-//   if (absDiff <= maxDiff) {
-//     almostEqual = true;
+// template <typename T> void printOutVector(std::vector<T> &customVector) {
+//   for (std::size_t iCoordinate = 0; iCoordinate < customVector.size();
+//        ++iCoordinate) {
+//     std::cout << customVector[iCoordinate] << "\n";
 //   }
-
-//   // Check if the numbers are really close when comparing numbers away from
-//   // zero
-//   auto expectedAbsValue = std::fabs(expectedValue);
-//   auto actualAbsValue = std::fabs(actualValue);
-//   auto largerAbsValue =
-//       (expectedAbsValue > actualAbsValue) ? expectedAbsValue :
-//       actualAbsValue;
-
-//   if (absDiff <= largerAbsValue * maxRelativeDiff) {
-//     almostEqual = true;
-//   }
-//   // almostEqual = false;
+//   std::cout << std::endl;
 // }
+
+bool AlmostEqualAbsAndRelative(const double &actualValue,
+                               const double &expectedValue,
+                               const double maxDiff,
+                               const double maxRelativeDiff) {
+
+  // Check if the numbers are really close when comparing not very small numbers
+  // or comparing against zero
+  double absDiff = std::fabs(actualValue - expectedValue);
+  if (absDiff <= maxDiff) {
+    return true;
+  }
+
+  // Check if the numbers are really close when comparing very small non-zero
+  // numbers
+  auto expectedAbsValue = std::fabs(expectedValue);
+  auto actualAbsValue = std::fabs(actualValue);
+
+  if (absDiff <= expectedAbsValue * maxRelativeDiff) {
+    return true;
+  }
+  return false;
+}
+
+template <typename T1, typename T2>
+::testing::AssertionResult
+VectorAlmostEqual(const T1 &actual, const T2 &expected, const std::size_t size,
+                  const double maxDiff, const double maxRelativeDiff) {
+  for (std::size_t i = 0; i < size; ++i) {
+    if (!AlmostEqualAbsAndRelative(actual[i], expected[i], maxDiff,
+                                   maxRelativeDiff)) {
+      return ::testing::AssertionFailure()
+             << "actual[" << i << "] (" << actual[i] << ") != expected[" << i
+             << "] (" << expected[i] << ")";
+    }
+  }
+  return ::testing::AssertionSuccess();
+}
 
 TEST(ProcessingOpenFoamMeshTest, ReadingPointsWorks) {
   // --- Arrange ---
   std::string caseDirectory("../../cases/elbow");
   readMesh meshReader;
   Mesh fvMesh(caseDirectory);
+  std::vector<double> face0_centroid = {1.0, 15.0, 0.0};
+  std::vector<double> face1_centroid = {53.0392360700000, -3.99307942350000,
+                                        0.0};
+  const double maxDiff = 1.0e-9;
+  const double maxRelativeDiff = 1.0e-4;
 
   // --- Act ---
   meshReader.readOpenFoamMesh(fvMesh);
 
   // --- Assert ---
   // Verify the number of nodes
-  EXPECT_EQ(fvMesh.nNodes(), 1074);
+  EXPECT_TRUE(VectorAlmostEqual(fvMesh.faces()[0].centroid(), face0_centroid, 3,
+                                maxDiff, maxRelativeDiff));
+
+  EXPECT_TRUE(VectorAlmostEqual(fvMesh.faces()[1].centroid(), face1_centroid, 3,
+                                maxDiff, maxRelativeDiff));
 }
