b88b599b8c2337cfce2e00b0633d957163eab056
Chih-Ta Wang <wangchihta@gmail.com>
2025-01-22 23:00:39 +0100

    Calculate the centroid, surface normal vector, and area of a polygon directly when the polygon is a triangle

--- src/processMesh.cpp
+++ src/processMesh.cpp
@@ -26,90 +26,126 @@ void processMesh::processOpenFoamMesh(Mesh &fvMesh) {
 void processMesh::processBasicFaceGeometry(Mesh &fvMesh) {
   for (std::size_t iFace = 0; iFace < fvMesh.nFaces(); ++iFace) {
 
-    std::array<double, 3> center = {0.0, 0.0, 0.0};
-    for (std::size_t iNode = 0; iNode < fvMesh.faces()[iFace].nNodes();
-         ++iNode) {
-      for (std::size_t iCoordinate = 0; iCoordinate < center.size();
-           ++iCoordinate)
-        center[iCoordinate] +=
-            fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[iNode]]
-                .centroid()[iCoordinate];
-    }
-
-    for (std::size_t iCoordinate = 0; iCoordinate < center.size();
-         ++iCoordinate) {
-      center[iCoordinate] /=
-          static_cast<double>(fvMesh.faces()[iFace].nNodes());
-    }
-
-    /*
-    // Using the center to compute the area and centroid of virtual
-    // triangles based on the center and the face nodes
-    */
-    std::array<double, 3> triangleNode1 = center;
-    std::array<double, 3> triangleNode2 = {0.0, 0.0, 0.0};
-    std::array<double, 3> triangleNode3 = {0.0, 0.0, 0.0};
     std::array<double, 3> centroid = {0.0, 0.0, 0.0};
     std::array<double, 3> Sf = {0.0, 0.0, 0.0};
     double area = 0.0;
 
-    // TO DO: Should consider a special case when the polygon is a triangle
-    for (std::size_t iNode = 0; iNode < fvMesh.faces()[iFace].nNodes();
-         ++iNode) {
-      triangleNode2 =
-          fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[iNode]].centroid();
-      if (iNode < fvMesh.faces()[iFace].nNodes() - 1) {
-        triangleNode3 =
-            fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[iNode + 1]]
-                .centroid();
-      } else {
-        triangleNode3 =
-            fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[0]].centroid();
-      }
+    // Consider a special case where the polygon is a triangle
+    if (fvMesh.faces()[iFace].nNodes() == 3) {
 
-      // Calculate the centroid of a given subtriangle
-      std::array<double, 3> local_centroid = {0.0, 0.0, 0.0};
-      for (std::size_t iCoordinate = 0; iCoordinate < local_centroid.size();
-           ++iCoordinate) {
-        local_centroid[iCoordinate] =
+      std::array<double, 3> &triangleNode1 =
+          fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[0]].centroid();
+      std::array<double, 3> &triangleNode2 =
+          fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[1]].centroid();
+      std::array<double, 3> &triangleNode3 =
+          fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[2]].centroid();
+
+      for (std::size_t iCoordinate = 0; iCoordinate < 3; ++iCoordinate) {
+        centroid[iCoordinate] =
             (triangleNode1[iCoordinate] + triangleNode2[iCoordinate] +
              triangleNode3[iCoordinate]) /
             3.0;
       }
 
-      // Calculate the surface area vector of a given subtriangle by cross
-      // product
-      std::array<double, 3> local_Sf = {0.0, 0.0, 0.0};
-      local_Sf[0] = 0.5 * ((triangleNode2[1] - triangleNode1[1]) *
-                               (triangleNode3[2] - triangleNode1[2]) -
-                           (triangleNode2[2] - triangleNode1[2]) *
-                               (triangleNode3[1] - triangleNode1[1]));
-      local_Sf[1] = 0.5 * ((triangleNode2[2] - triangleNode1[2]) *
-                               (triangleNode3[0] - triangleNode1[0]) -
-                           (triangleNode2[0] - triangleNode1[0]) *
-                               (triangleNode3[2] - triangleNode1[2]));
-      local_Sf[2] = 0.5 * ((triangleNode2[0] - triangleNode1[0]) *
-                               (triangleNode3[1] - triangleNode1[1]) -
-                           (triangleNode2[1] - triangleNode1[1]) *
-                               (triangleNode3[0] - triangleNode1[0]));
+      // Calculate the area of a given triangle
+      Sf[0] = 0.5 * ((triangleNode2[1] - triangleNode1[1]) *
+                         (triangleNode3[2] - triangleNode1[2]) -
+                     (triangleNode2[2] - triangleNode1[2]) *
+                         (triangleNode3[1] - triangleNode1[1]));
+      Sf[1] = 0.5 * ((triangleNode2[2] - triangleNode1[2]) *
+                         (triangleNode3[0] - triangleNode1[0]) -
+                     (triangleNode2[0] - triangleNode1[0]) *
+                         (triangleNode3[2] - triangleNode1[2]));
+      Sf[2] = 0.5 * ((triangleNode2[0] - triangleNode1[0]) *
+                         (triangleNode3[1] - triangleNode1[1]) -
+                     (triangleNode2[1] - triangleNode1[1]) *
+                         (triangleNode3[0] - triangleNode1[0]));
+      area = mag(Sf);
 
-      // Calculate the surface area of a given subtriangle
-      double local_area = mag(local_Sf);
+    } else {
+      std::array<double, 3> center = {0.0, 0.0, 0.0};
+      for (std::size_t iNode = 0; iNode < fvMesh.faces()[iFace].nNodes();
+           ++iNode) {
+        for (std::size_t iCoordinate = 0; iCoordinate < center.size();
+             ++iCoordinate)
+          center[iCoordinate] +=
+              fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[iNode]]
+                  .centroid()[iCoordinate];
+      }
 
+      for (std::size_t iCoordinate = 0; iCoordinate < center.size();
+           ++iCoordinate) {
+        center[iCoordinate] /=
+            static_cast<double>(fvMesh.faces()[iFace].nNodes());
+      }
+
+      /*
+      // Using the center to compute the area and centroid of virtual
+      // triangles based on the center and the face nodes
+      */
+      std::array<double, 3> triangleNode1 = center;
+      std::array<double, 3> triangleNode2 = {0.0, 0.0, 0.0};
+      std::array<double, 3> triangleNode3 = {0.0, 0.0, 0.0};
+      // std::array<double, 3> centroid = {0.0, 0.0, 0.0};
+      // std::array<double, 3> Sf = {0.0, 0.0, 0.0};
+      // double area = 0.0;
+
+      for (std::size_t iNode = 0; iNode < fvMesh.faces()[iFace].nNodes();
+           ++iNode) {
+        triangleNode2 =
+            fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[iNode]].centroid();
+        if (iNode < fvMesh.faces()[iFace].nNodes() - 1) {
+          triangleNode3 =
+              fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[iNode + 1]]
+                  .centroid();
+        } else {
+          triangleNode3 =
+              fvMesh.nodes()[fvMesh.faces()[iFace].iNodes()[0]].centroid();
+        }
+
+        // Calculate the centroid of a given subtriangle
+        std::array<double, 3> local_centroid = {0.0, 0.0, 0.0};
+        for (std::size_t iCoordinate = 0; iCoordinate < local_centroid.size();
+             ++iCoordinate) {
+          local_centroid[iCoordinate] =
+              (triangleNode1[iCoordinate] + triangleNode2[iCoordinate] +
+               triangleNode3[iCoordinate]) /
+              3.0;
+        }
+
+        // Calculate the surface area vector of a given subtriangle by cross
+        // product
+        std::array<double, 3> local_Sf = {0.0, 0.0, 0.0};
+        local_Sf[0] = 0.5 * ((triangleNode2[1] - triangleNode1[1]) *
+                                 (triangleNode3[2] - triangleNode1[2]) -
+                             (triangleNode2[2] - triangleNode1[2]) *
+                                 (triangleNode3[1] - triangleNode1[1]));
+        local_Sf[1] = 0.5 * ((triangleNode2[2] - triangleNode1[2]) *
+                                 (triangleNode3[0] - triangleNode1[0]) -
+                             (triangleNode2[0] - triangleNode1[0]) *
+                                 (triangleNode3[2] - triangleNode1[2]));
+        local_Sf[2] = 0.5 * ((triangleNode2[0] - triangleNode1[0]) *
+                                 (triangleNode3[1] - triangleNode1[1]) -
+                             (triangleNode2[1] - triangleNode1[1]) *
+                                 (triangleNode3[0] - triangleNode1[0]));
+
+        // Calculate the surface area of a given subtriangle
+        double local_area = mag(local_Sf);
+
+        for (std::size_t iCoordinate = 0; iCoordinate < centroid.size();
+             ++iCoordinate) {
+          centroid[iCoordinate] += local_area * local_centroid[iCoordinate];
+          Sf[iCoordinate] += local_Sf[iCoordinate];
+        }
+      }
+      area = mag(Sf);
+
+      // Compute centroid of the polygon
       for (std::size_t iCoordinate = 0; iCoordinate < centroid.size();
            ++iCoordinate) {
-        centroid[iCoordinate] += local_area * local_centroid[iCoordinate];
-        Sf[iCoordinate] += local_Sf[iCoordinate];
+        centroid[iCoordinate] /= area;
       }
     }
-    area = mag(Sf);
-
-    // Compute centroid of the polygon
-    for (std::size_t iCoordinate = 0; iCoordinate < centroid.size();
-         ++iCoordinate) {
-      centroid[iCoordinate] /= area;
-    }
-
     fvMesh.faces()[iFace].centroid() = centroid;
     fvMesh.faces()[iFace].Sf() = Sf;
     fvMesh.faces()[iFace].area() = area;
