c0f44a7e808ae190851a5c4db85d1ef3a4ff82d6
chihta-wang <wangchihta@gmail.com>
2025-03-14 10:00:27 +0100

    Improve the comment in the Field class

--- include/Field.hpp
+++ include/Field.hpp
@@ -13,7 +13,7 @@ public:
   std::vector<double> &values() { return values_; }
   const std::size_t size() { return size_; }
 
-  // Set the values of a scalar field to 1.0
+  // Set all the values of a scalar field to a given value (default is 1.0)
   void setValues(const double value = 1.0) {
     for (std::size_t iElement = 0; iElement < size(); ++iElement) {
       values()[iElement] = value;
