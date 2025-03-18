6424f834b0ea09eaf102142dc28fad2641da83bd
Chih-Ta Wang <wangchihta@gmail.com>
2025-03-18 16:51:43 +0100

    In the Field class, use std vector assignment operations instead of assigning values by a loop

--- include/Field.hpp
+++ include/Field.hpp
@@ -15,11 +15,7 @@ public:
   const std::size_t size() { return values_.size(); }
 
   // Set all the values of a field to a given value
-  void set(const T &value) {
-    for (std::size_t iElement = 0; iElement < values_.size(); ++iElement) {
-      values_[iElement] = value;
-    }
-  }
+  void set(const T &value) { values_.assign(values_.size(), value); }
 
   std::string &boundaryType() { return boundaryType_; }
 
