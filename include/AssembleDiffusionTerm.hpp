35494b92a9cfed24735a6bf5f49b012d11c42908
chihta-wang <wangchihta@gmail.com>
2025-04-15 11:39:13 +0200

    Calculate the coefficient matrix and the right-hand side via a cell-based loop for discretization of diffusion equation

--- include/AssembleDiffusionTerm.hpp
+++ include/AssembleDiffusionTerm.hpp
@@ -1,15 +1,19 @@
 #ifndef ASSERMBLE_DIFF_HPP
 #define ASSERMBLE_DIFF_HPP
 
-// #include "Field.hpp"
+#include "Field.hpp"
 #include "Mesh.hpp"
 
 class AssembleDiffusionTerm {
 public:
   // Assemble the diffusion term on a Cartesian orthogonal mesh by looping over
   // the elements (cells)
-  void AssembleDiffusionTerm::elementBasedAssemble(Mesh &fvMesh,
-                                                   const double *diffusionCoef);
+  void elementBasedAssemble(Mesh &fvMesh,
+                            const std::vector<double> diffusionCoef,
+                            const std::vector<double> &Q,
+                            std::vector<boundaryField<double>> &boundaryFields,
+                            std::vector<std::vector<double>> &coeffMatrix,
+                            std::vector<double> &RHS);
 
   // Assemble the diffusion term on a Cartesian orthogonal mesh by looping over
   // the faces
--- src/AssembleDiffusionTerm.cpp
+++ src/AssembleDiffusionTerm.cpp
@@ -1,20 +1,27 @@
 #include "AssembleDiffusionTerm.hpp"
+#include "Field.hpp"
 #include <cstddef>
-#include <vector>
 
-void AssembleDiffusionTerm::elementBasedAssemble(Mesh &fvMesh,
-                                                 const double *diffusionCoef) {
+void AssembleDiffusionTerm::elementBasedAssemble(
+    Mesh &fvMesh, const std::vector<double> diffusionCoef,
+    const std::vector<double> &Q,
+    std::vector<boundaryField<double>> &boundaryFields,
+    std::vector<std::vector<double>> &coeffMatrix, std::vector<double> &RHS) {
 
   const std::size_t nElements = fvMesh.nElements();
 
   // Declare a 2D array for coefficient matrix corresponding to the discrete
   // form of diffusion equation
-  std::vector<std::vector<double>> coeffMatrix(
-      nElements, std::vector<double>(nElements, 0.0));
+  // std::vector<std::vector<double>> coeffMatrix(
+  //     nElements, std::vector<double>(nElements, 0.0));
 
   for (std::size_t iElement = 0; iElement < nElements; ++iElement) {
     // Get the element
     Element &theElement = fvMesh.elements()[iElement];
+
+    // Calculate the source term and it to the RHS
+    RHS[iElement] = Q[iElement] * theElement.volume();
+
     const std::size_t nFaces = theElement.iFaces().size();
 
     for (std::size_t iFace = 0; iFace < nFaces; ++iFace) {
@@ -37,14 +44,29 @@ void AssembleDiffusionTerm::elementBasedAssemble(Mesh &fvMesh,
         coeffMatrix[iElement][iElement] += FluxCn;
 
       } else { // If it is a boundary face
-        // Get the boundary condition
-        // if () { // Dirichlet BC
+        // Get the boundary type
+        const std::size_t iBoundary = fvMesh.faces()[iFaceIndex].patchIndex();
+        const std::string &boundaryType =
+            boundaryFields[iBoundary].boundaryType();
 
-        // }
+        double FluxCb = 0.0;
+        double FluxVb = 0.0;
 
-        // else if () { // Neumann BC
+        if (boundaryType == "fixedValue") { // Dirichlet BC
+          FluxCb = diffusionCoef[iFaceIndex] * theFace.gDiff();
 
-        // }
+          std::size_t relativeFaceIndex =
+              iFaceIndex - fvMesh.boundaries()[iBoundary].startFace();
+          FluxVb =
+              -FluxCb * boundaryFields[iBoundary].values()[relativeFaceIndex];
+        }
+
+        else if (boundaryType == "zeroGradient") { // zero Neumann BC
+          // Do nothing because FluxCb and FluxVb are already 0.0
+        }
+
+        coeffMatrix[iElement][iElement] += FluxCb;
+        RHS[iElement] -= FluxVb;
 
         // else if () { // mixed BC
         // }
