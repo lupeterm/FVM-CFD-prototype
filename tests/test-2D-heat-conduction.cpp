c225689fed5cea8815d476453c8e5be3accd599b
chihta-wang <wangchihta@gmail.com>
2025-04-23 12:06:46 +0200

    Add a test case of 2D heat conduction equation on a 3x4 mesh Add a unit test accordinlgy

--- tests/test-2D-heat-conduction.cpp
+++ tests/test-2D-heat-conduction.cpp
@@ -13,7 +13,7 @@
 // ****** Tests ******
 TEST(ReadingMeshFor2DHeatConductionTest, ReadingPointsWorks) {
   // --- Arrange ---
-  std::string caseDirectory("../../cases/2D-heat-conduction");
+  std::string caseDirectory("../../cases/2D-heat-conduction-on-a-2-by-2-mesh");
   Mesh fvMesh(caseDirectory);
   ReadMesh meshReader;
 
@@ -36,7 +36,7 @@ TEST(ReadingMeshFor2DHeatConductionTest, ReadingPointsWorks) {
 
 TEST(ReadingMeshFor2DHeatConductionTest, ConfirmingElementOrdering) {
   // --- Arrange ---
-  std::string caseDirectory("../../cases/2D-heat-conduction");
+  std::string caseDirectory("../../cases/2D-heat-conduction-on-a-2-by-2-mesh");
   Mesh fvMesh(caseDirectory);
   ReadMesh meshReader;
 
@@ -74,7 +74,7 @@ TEST(ReadingMeshFor2DHeatConductionTest, ConfirmingElementOrdering) {
 TEST(ReadInitialBoundaryConditionsTest, ReadingBoundaryTemperatureFieldWorks) {
 
   // --- Arrange ---
-  std::string caseDirectory("../../cases/2D-heat-conduction");
+  std::string caseDirectory("../../cases/2D-heat-conduction-on-a-2-by-2-mesh");
   Mesh fvMesh(caseDirectory);
   ReadMesh meshReader;
 
@@ -139,7 +139,7 @@ TEST(ReadInitialBoundaryConditionsTest, ReadingBoundaryTemperatureFieldWorks) {
 
 TEST(SortBoundaryFacesFromInteriorFacesTest, LabelingBoundaryFacesWorks) {
   // --- Arrange ---
-  std::string caseDirectory("../../cases/2D-heat-conduction");
+  std::string caseDirectory("../../cases/2D-heat-conduction-on-a-2-by-2-mesh");
   Mesh fvMesh(caseDirectory);
   ReadMesh meshReader;
 
@@ -165,9 +165,91 @@ TEST(SortBoundaryFacesFromInteriorFacesTest, LabelingBoundaryFacesWorks) {
   EXPECT_EQ(fvMesh.faces()[19].patchIndex(), 4);
 }
 
-TEST(DiscretizingDiffusionTermTest, Discretizing2DHeatConductionWorks) {
+TEST(DiscretizingDiffusionTermTest,
+     Discretizing2DHeatConductionOn2By2MeshWorks) {
   // --- Arrange ---
-  std::string caseDirectory("../../cases/2D-heat-conduction");
+  std::string caseDirectory("../../cases/2D-heat-conduction-on-a-2-by-2-mesh");
+  Mesh fvMesh(caseDirectory);
+  ReadMesh meshReader;
+  meshReader.readOpenFoamMesh(fvMesh);
+
+  // Define the thermal conductivity and source term
+  std::vector<double> thermalConductivity(fvMesh.nFaces(), 1.0);
+  std::vector<double> heatSource(fvMesh.nElements(), 0.0);
+
+  // Read initial condition and boundary conditions
+  Field<double> internalTemperatureField(fvMesh.nElements());
+  std::vector<boundaryField<double>> boundaryTemperatureFields;
+  ReadInitialBoundaryConditions initialBoundaryConditionsReader;
+  initialBoundaryConditionsReader.readTemperatureField(
+      fvMesh, internalTemperatureField, boundaryTemperatureFields);
+
+  // Define the coefficient matrix and RHS vector
+  //   Matrix<double> coeffMatrix(fvMesh.nElements(), fvMesh.nElements(), 0.0);
+  //   std::vector<std::vector<double>> coeffMatrix(
+  //       fvMesh.nElements(), std::vector<double>(fvMesh.nElements(), 0.0));
+  Matrix<double> coeffMatrix(fvMesh.nElements(), fvMesh.nElements(), 0.0);
+  std::vector<double> RHS(fvMesh.nElements(), 0.0);
+
+  // Set up expected values for the coefficient matrix and RHS vector
+
+  //   const std::array<double, 3> expected_element0_centroid = {
+  //       0.250000000000000, 0.250000000000000, 0.500000000000000};
+  //   const std::array<double, 3> expected_element1_centroid = {
+  //       0.750000000000000, 0.250000000000000, 0.500000000000000};
+  //   const std::array<double, 3> expected_element2_centroid = {
+  //       0.250000000000000, 0.750000000000000, 0.500000000000000};
+  //   const std::array<double, 3> expected_element3_centroid = {
+  //       0.750000000000000, 0.750000000000000, 0.500000000000000};
+
+  //   const double maxDiff = 1.0e-9;
+  //   const double maxRelativeDiff = 1.0e-4;
+
+  // --- Act ---
+  AssembleDiffusionTerm diffusionTermAssembler;
+  diffusionTermAssembler.elementBasedAssemble(
+      fvMesh, thermalConductivity, heatSource, boundaryTemperatureFields,
+      coeffMatrix, RHS);
+
+  // Print out coeffMatrix for debugging
+  std::cout << "Coefficient Matrix:" << std::endl;
+  for (std::size_t i = 0; i < coeffMatrix.nRows(); ++i) {
+    for (std::size_t j = 0; j < coeffMatrix.nCols(); ++j) {
+      std::cout << coeffMatrix(i, j) << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  // Print out RHS for debugging
+  std::cout << "RHS Vector:" << std::endl;
+  for (std::size_t i = 0; i < RHS.size(); ++i) {
+    std::cout << RHS[i] << std::endl;
+  }
+
+  // --- Assert ---
+  // Verify the coefficient matrix
+
+  //   EXPECT_EQ(coeffMatrix[0][0], 4);
+  // EXPECT_TRUE(ScalarAlmostEqual
+  //   std::cout << "coeffMatrix[0][0]: " << coeffMatrix[0][0] << std::endl;
+  //   EXPECT_TRUE(VectorAlmostEqual(fvMesh.elements()[0].centroid(),
+  //                                 expected_element0_centroid, 3, maxDiff,
+  //                                 maxRelativeDiff));
+  //   EXPECT_TRUE(VectorAlmostEqual(fvMesh.elements()[1].centroid(),
+  //                                 expected_element1_centroid, 3, maxDiff,
+  //                                 maxRelativeDiff));
+  //   EXPECT_TRUE(VectorAlmostEqual(fvMesh.elements()[2].centroid(),
+  //                                 expected_element2_centroid, 3, maxDiff,
+  //                                 maxRelativeDiff));
+  //   EXPECT_TRUE(VectorAlmostEqual(fvMesh.elements()[3].centroid(),
+  //                                 expected_element3_centroid, 3, maxDiff,
+  //                                 maxRelativeDiff));
+}
+
+TEST(DiscretizingDiffusionTermTest,
+     Discretizing2DHeatConductionOn3By3MeshWorks) {
+  // --- Arrange ---
+  std::string caseDirectory("../../cases/2D-heat-conduction-on-a-3-by-3-mesh");
   Mesh fvMesh(caseDirectory);
   ReadMesh meshReader;
   meshReader.readOpenFoamMesh(fvMesh);
