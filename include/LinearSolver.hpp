8cdc8de0df51a9048763a203ffe0d0309db0c60a
chihta-wang <wangchihta@gmail.com>
2025-04-29 13:12:50 +0200

    Finish the implementation of the solve function of the LinearSolver class The solve function uses a CG iterative solver from the Ginkgo library

--- include/LinearSolver.hpp
+++ include/LinearSolver.hpp
@@ -7,8 +7,14 @@
 class LinearSolver {
 public:
   // Solve the linear system of equations
-  void solve(gko::matrix_data<double, int> &coeffMatrix,
-             std::vector<double> &RHS, std::vector<double> &solVector);
+  template <typename ValueType, typename IndexType>
+  void solve(gko::matrix_data<ValueType, IndexType> &coeffMatrix,
+             std::vector<ValueType> &RHS, std::vector<ValueType> &solVector,
+             gko::remove_complex<ValueType> reduction_factor,
+             IndexType maxNumIterations);
+
+  // template <typename ValueType>
+  // void print_solution(const std::vector<ValueType> &solVector) const;
 
 private:
   // Add any private member variables or helper functions if needed
--- src/LinearSolver.cpp
+++ src/LinearSolver.cpp
@@ -1,39 +1,69 @@
 #include "LinearSolver.hpp"
-// #include <ginkgo/ginkgo.hpp> // Required for Ginkgo library
-#include <stdexcept> // Required for std::invalid_argument
+#include <iostream>
 
-void LinearSolver::solve(gko::matrix_data<double, int> &coeffMatrix,
-                         std::vector<double> &RHS,
-                         std::vector<double> &solVector) {
+template <typename ValueType, typename IndexType>
+void LinearSolver::solve(gko::matrix_data<ValueType, IndexType> &coeffMatrix,
+                         std::vector<ValueType> &RHS,
+                         std::vector<ValueType> &solVector,
+                         gko::remove_complex<ValueType> reduction_factor,
+                         IndexType maxNumIterations) {
   // Create an executor (e.g., CPU executor)
   auto exec = gko::ReferenceExecutor::create();
 
   // === Prepare the Ginkgo matrix and vectors to be used by a Ginkgo solver ===
   // --- Convert the input matrix coeffMatrix to a Ginkgo matrix ---
-  using IndexType = int; // int or int64
-  using ValueType = double;
+  //   using IndexType = int; // int or int64
+  //   using ValueType = double;
   using mtx = gko::matrix::Coo<ValueType, IndexType>;
   using vec = gko::matrix::Dense<ValueType>;
   using val_array = gko::array<ValueType>;
 
   coeffMatrix.sort_row_major();
 
-  auto gko_coeffMatrix = mtx::create(exec);
+  auto gko_coeffMatrix = gko::share(mtx::create(exec));
   gko_coeffMatrix->read(coeffMatrix);
 
   // --- Convert the input RHS vector to a Ginkgo vector ---
-  const auto &nMeshPoints = RHS.size();
-
   // executor where the application initialized the data
   const auto app_exec = exec->get_master();
+  const auto &nMeshPoints = RHS.size();
   auto gko_RHS =
       vec::create(exec, gko::dim<2>(nMeshPoints, 1),
                   val_array::view(app_exec, nMeshPoints, RHS.data()), 1);
 
-  std::vector<ValueType> x0(nMeshPoints, 0.0);
+  // --- Create a Ginkgo matrix to store the solution vector ---
+  // Create an initial guess vector
+  //   std::vector<ValueType> x0(nMeshPoints, 0.0);
   auto gko_x =
       vec::create(app_exec, gko::dim<2>(nMeshPoints, 1),
-                  val_array::view(app_exec, nMeshPoints, x0.data()), 1);
+                  val_array::view(app_exec, nMeshPoints, solVector.data()), 1);
 
   // === Solve the linear system of equations ===
+  // --- Generate solver ---
+  // Generate a Ginkgo CG solver with the block Jacobi preconditioner
+  using cg = gko::solver::Cg<ValueType>;
+  using bj = gko::preconditioner::Jacobi<ValueType, IndexType>;
+
+  auto solver_factory =
+      cg::build()
+          .with_criteria(
+              gko::stop::Iteration::build().with_max_iters(
+                  gko::size_type(maxNumIterations)),
+              gko::stop::ResidualNorm<ValueType>::build().with_reduction_factor(
+                  reduction_factor))
+          .with_preconditioner(bj::build())
+          .on(exec);
+  auto solver = solver_factory->generate(gko_coeffMatrix);
+
+  // --- Solve system ---
+  solver->apply(gko_RHS, gko_x);
 }
+
+// template <typename ValueType>
+// void LinearSolver::print_solution(
+//     const std::vector<ValueType> &solVector) const {
+//   std::cout << "Solution vector:" << std::endl;
+//   for (std::size_t i = 0; i < solVector.size(); ++i) {
+//     std::cout << "x[" << i << "] = " << solVector[i] << std::endl;
+//   }
+// }
